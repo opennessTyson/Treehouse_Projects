9a703ff82fc5b21d738a8ab2291cdbe25833b32b
s3756051 <s3756051@student.rmit.edu.au>
2020-12-06 18:21:12 +0100

    Added highscore.

--- Unit_1_Guessing_Game/guessing_game.py
+++ Unit_1_Guessing_Game/guessing_game.py
@@ -1,10 +1,13 @@
 import random
 
+name = input("Hello Friend. Welcome to Guessing Game, what should we call you?:  ")
+
 def start_game():
     random_number = random.randrange(1,11)
+    high_score = []
     attempts = 1
     guess = 0
-    name = input("Hello Friend. Welcome to Guessing Game, what should we call you?:  ")
+    
     while  guess !=  random_number:
         try:
             guess = int(input("{}, guess a number between 0-10.  ".format(name)))
@@ -19,13 +22,15 @@ def start_game():
             elif guess >random_number:
                 print("It's lower")
                 attempts += 1
-            elif guess == random_number:                 
+            elif guess == random_number:
+                high_score.append(attempts)
                 print("Got it")
                 print("You are too good. Game over in {} attempts. ".format(attempts))
     while True:
         if guess == random_number:
                 play_again = input("Would you like to play again? Y/N?")
                 if play_again.lower() == "y":
+                    print("Current high score is {} attempts. ".format(min(high_score)))
                     start_game()
                     continue
                 elif play_again.lower() == "n":
